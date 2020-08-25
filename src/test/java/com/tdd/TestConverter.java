45d0511b0608430a67fb0f47bd2f758d36591eae
Ko4AY <lexus-ka@yandex.ru>
2020-08-25 19:24:12 +0500

    Test for arabic 9

--- src/test/java/com/tdd/TestConverter.java
+++ src/test/java/com/tdd/TestConverter.java
@@ -9,12 +9,21 @@ import static org.mockito.Mockito.*;
 public class TestConverter {
 
     @Test
-    public void arabicToRoman() {
+    public void arabicToRoman_whenArabicOneIsI_thenOk() {
         Integer arabic = 1;
 
         String roman = Converter.arabicToRoman(arabic);
 
         assertEquals(roman, "I");
+    }
+
+    @Test
+    public void arabicToRoman_whenArabicNineIsIX_thenOk() {
+        Integer arabic = 9;
+
+        String roman = Converter.arabicToRoman(arabic);
+
+        assertEquals(roman, "IX");
     }
 
 }
